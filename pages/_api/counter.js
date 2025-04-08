e8d719377f9255cde0516e3b0f4b615bbc932189
Creambeam <andy.gallagher0714@gmail.com>
2025-04-08 13:47:41 -0500

    Create counter.js

---
+++ pages/_api/counter.js
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+let counter = 0;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    return res.json({ counter });
+  } else if (req.method === 'POST') {
+    counter++;
+    return res.json({ counter });
+  } else {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+}
