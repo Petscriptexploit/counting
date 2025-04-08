842108553e0fa8033253cffe5d8f886cf64f1f37
Creambeam <andy.gallagher0714@gmail.com>
2025-04-08 13:48:21 -0500

    Create _index.js

---
+++ pages/_api/_index.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+export default function Home() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    fetch('/api/counter')
+      .then(response => response.json())
+      .then(data => setCounter(data.counter));
+  }, []);
+
+  const handleIncrement = async () => {
+    const response = await fetch('/api/increment', { method: 'POST' });
+    const data = await response.json();
+    setCounter(data.counter);
+  };
+
+  return (
+    <div>
+      <button onClick={handleIncrement}>Click me!</button>
+      <p>Counter: {counter}</p>
+    </div>
+  );
+}
