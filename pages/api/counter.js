40fc03a51e3d052240aca859b72c85d049fc1783
Creambeam <andy.gallagher0714@gmail.com>
2025-04-08 14:10:00 -0500

    Update counter.js

--- pages/api/counter.js
+++ pages/api/counter.js
@@ -1,5 +1,27 @@
 // pages/api/counter.js
+import { NextApiRequest, NextApiResponse } from 'next';
+import WebSocket from 'ws';
+
+const wss = new WebSocket.Server({ port: 3000 });
+
 let counter = 0;
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    const data = JSON.parse(message);
+
+    if (data.type === 'increment') {
+      counter++;
+      ws.send(JSON.stringify({ counter }));
+    }
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
